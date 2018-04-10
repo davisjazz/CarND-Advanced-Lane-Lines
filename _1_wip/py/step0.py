c238ed7e452a03af3f3cebcc837117f9717f1cd1
unknown <davisjazz@gmail.com>
2018-04-10 20:15:23 +0200

    [step1] camera calibration using chessboard images, modules, ipynb . rename 'module' the 'py' directory . step0.py: 180410-2005   - update fct parse_args()     .. note: imported into other modules (*.py)     .. add args.to_plot   - add class PARSE_ARGS(), note: imported into < _prj04_180410-1252.ipynb >   - add images_plot() . step1.py: 180410-1443   - implement args.to_plot . _prj04_180410-2007.ipynb: 180410-2008   - import from modules: step0, step1   - show images_corners_found, images_corners_not_found
    . bug:
      - image_undistort() KO bcs chessboard corners not found (ret = False)

--- _1_wip/py/step0.py
+++ _1_wip/py/step0.py
@@ -19,22 +19,68 @@ default_out  = default_root + '_1_INPUT/CarND-Advanced-Lane-Lines-master/output_
 default_test = default_root + '_1_INPUT/CarND-Advanced-Lane-Lines-master/test_images/'
 
 # Helper function: command-line / parse parameters 
-def PARSE_ARGS():
-    # a b d f g h i j k l m n q r u v w x y z
+def parse_args():
+    # a b f g h i k l m n q r u v w x y z
     parser = argparse.ArgumentParser(prog='advanced techniques for lane finding', description='advanced techniques for lane finding')
     parser.add_argument('-p', '--path',	  dest='path', help='root directory path', action='store', type=str, default=default_dir)
     parser.add_argument('-s', '--sand',	  dest='sand', help='sandbox directory path', action='store', type=str, default=default_sand)
     parser.add_argument('-c', '--cali',	  dest='cali', help='calibration images directory path', action='store', type=str, default=default_cali)
-    parser.add_argument('-e', '--exp',	  dest='cali', help='calibration images directory path', action='store', type=str, default=default_exp)
-    parser.add_argument('-o', '--out',	  dest='cali', help='calibration images directory path', action='store', type=str, default=default_out)
-    parser.add_argument('-t', '--test',	  dest='cali', help='calibration images directory path', action='store', type=str, default=default_test)
-    parser.add_argument('-i', '--incorner', dest='tab', help='table size', action='store', type=list, default=[9,6])
-
+    parser.add_argument('-e', '--exp',	  dest='exp', help='calibration images directory path', action='store', type=str, default=default_exp)
+    parser.add_argument('-o', '--out',	  dest='out', help='calibration images directory path', action='store', type=str, default=default_out)
+    parser.add_argument('-t', '--test',	  dest='test', help='calibration images directory path', action='store', type=str, default=default_test)
+    parser.add_argument('-i', '--incorner', dest='incorner', help='table size', action='store', type=list, default=[9,6])
+    parser.add_argument('-j', '--column', dest='column', help='plot images in a table of n columns', action='store', type=int, default=5)
+    parser.add_argument('-d', '--display', dest='to_plot', help='activate the listing of images to plot - by default the listing is not activated', action='store_true')
+    # TODO: delete the two following lines in comment
+    #parser.add_argument('-t', '--tune', help='activate the fine tune mode - by default it is in training mode', dest='tune', action='store_true', default=False)
+    #parser.add_argument('-f', '--freeze', help='freeze all layers except the last one when the fine tune mode is activated - by default these layers are unfrozen', dest='freeze', action='store_false')
     args   = parser.parse_args()
     # a b c d e f g h i j k l m n o p q r s t u v w x y z
     return args
 
 
+class PARSE_ARGS(object):
+    def __init__(self,
+                 path = default_dir,
+                 sand = default_sand,
+                 cali = default_cali,
+                 exp  = default_exp,
+                 out  = default_out,
+                 test = default_test,
+                 incorner = [9,6],
+                 column   = 5,
+                 to_plot  = False):
+
+        self.path = path # root directory path
+        self.sand = sand # sandbox directory path
+        self.cali = cali # calibration images directory path
+        self.exp  = exp  #
+        self.out  = out  #
+        self.test = test #
+        self.incorner = incorner #
+        self.column   = column # 
+        self.to_plot  = to_plot
+
+    def path(self):
+        return self.path
+    def sand(self):
+        return self.sand
+    def cali(self):
+        return self.cali
+    def exp(self):
+        return self.exp
+    def out(self):
+         return self.out
+    def test(self):
+        return self.test
+    def incorner(self):
+        return self.incorner
+    def column(self):
+        return self.column
+    def to_plot(self):
+        return self.to_plot
+
+# TODO: delete steps()
 # Helper function: remind me the steps
 def steps():
     steps = ['#STEPS',
@@ -54,19 +100,83 @@ def steps():
             count += 1
 
 
+# TODO: delete template_function()
+def template_function():
+    '''
+    Summary line.
+
+    Extended description of function.
+
+    Parameters
+    ----------
+    arg1 : int
+        Description of arg1
+    arg2 : str
+        Description of arg2
+
+    Returns
+    -------
+    int
+        Description of return value
+
+    ref: http://docs.python-guide.org/en/latest/writing/documentation/
+    '''
+
+    pass
+
+
+# Helper function: show images
+def images_plot(args, images_directory):
+    '''
+    #parameters:
+    . xxx:
+        -
+    #return:
+    . show images
+    '''
+    # parameters
+    image_quantity = len(images_directory)
+
+    if image_quantity%args.column == 0:
+        row = image_quantity//args.column
+    else:
+        row = 1 + (image_quantity//args.column)
+
+    # show images
+    figure, axis = plt.subplots(figsize=(15.0, 13.0), nrows=row, ncols=args.column)  # (15.0, 10.0)
+    figure.subplots_adjust(hspace=0.2, wspace=0.1)  # hspace=0.2, wspace=0.1) # hspace=0.05, wspace=0.05
+    axis = axis.ravel()
+
+    for frame in range(row * args.column):
+        if frame < image_quantity:
+            # read in a calibration frame
+            image = mpimg.imread(images_directory[frame])
+            axis[frame].imshow(image)
+            axis[frame].axis('off')
+            title = images_directory[frame].split('\\')[-1]
+            axis[frame].set_title(title, fontsize=8)
+        else:
+            axis[frame].axis('off')
+
+
+
 def main():
     # parameters and placeholders
-    args  = PARSE_ARGS()
+    args  = parse_args()
+    flags = PARSE_ARGS()
 
     # # make a list of calibration images
     # fname = args.cali+'GOPR0042.jpg'
     # img = cv2.imread(fname)
     # plt.imshow(img)
 
-    img = mpimg.imread(default_sand+'traffic_sign_stop.jpg')
-    plt.imshow(img)
-    plt.show()
+    # img = mpimg.imread(default_sand+'traffic_sign_stop.jpg')
+    # plt.imshow(img)
+    # plt.show()
     steps()
+    print(args.to_plot)
+    print(flags.to_plot)
+
 
 if __name__ == '__main__':
     main()
--- _1_wip/py/step1.py
+++ _1_wip/py/step1.py
@@ -1,4 +1,4 @@
-from step0 import PARSE_ARGS, steps 
+from module.step0 import PARSE_ARGS, steps
 import argparse
 import os
 import pickle
@@ -14,6 +14,28 @@ import glob
 
 # Helper function: camera calibration
 def camera_calibrate(args):
+    """
+    compute the camera matrix and distortion coefficients
+
+    #parameters
+    . args.incorner : two dimensional list of int
+        - args.incorner[0] is the number of inside corners in x
+        - args.incorner[1] is the number of inside corners in y
+    . args.cali : str
+        - calibration images directory
+
+    #returns
+    . camera_matrix : xxx
+        - camera metrics to transform 3D objects to 2D images
+    . coef_distorsion:
+        - distorsion coefficient
+    """
+    # TODO: delete the following comments
+    # . objpoints: list
+    #     - 3D points in real world space (args.incorner[0] * args.incorner[1])
+    # . imgpoints: list
+    #     - 2D points in image plane ( chessboard corners )
+
     # prepare object points
     nx, ny = args.incorner #number of inside corners in x, in y
 
@@ -22,6 +44,8 @@ def camera_calibrate(args):
 
     # arrays to store object points and image points from all the images
     objpoints, imgpoints = [], [] # 3D points in real world space, 2D points in image plane
+    if args.to_plot:
+        image_corners_found,  image_corners_not_found = [], []
 
     # prepare object points, like (0,0,0) , (1,0,0) , (2,0,0) ..., (7,5,0)
     objp = np.zeros((nx*ny, 3), np.float32)
@@ -29,10 +53,10 @@ def camera_calibrate(args):
 
     for frame in images:
         # read in a calibration frame
-        image = mpimg.imread(frame)
+        image = mpimg.imread(frame) # RGB for moviepy
 
         # convert frame to grayscale
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
         # find the chessboard corners
         ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
@@ -41,16 +65,36 @@ def camera_calibrate(args):
         if ret == True:
             imgpoints.append(corners)
             objpoints.append(objp)
+            if args.to_plot: image_corners_found.append(frame)
+        else:
+            if args.to_plot: image_corners_not_found.append(frame)
 
-            # # draw and display the corners
-            # image = cv2.drawChessboardCorners(image, (nx, ny), corners, ret)
-            # plt.imshow(image)
-            # plt.show()
 
-    return objpoints, imgpoints
+    # TODO: delete the following line
+    #return objpoints, imgpoints
+
+    # compute the camera matrix, distortion coefficients, rotation and translation vectors
+    # - reprojection_error : the root mean square (RMS) re-projection error
+    # - camera_matrix : camera metrics to transform 3D objects to 2D images
+    # - coef_distorsion: distorsion coefficient
+    # - rvecs, tvecs: location of the camera in the world (r: rotation, t: translation)
+    reprojection_error, camera_matrix, coef_distorsion, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    if args.to_plot:
+        return [camera_matrix, coef_distorsion], [image_corners_found,  image_corners_not_found]
+    else:
+        return [camera_matrix, coef_distorsion]
 
 # Helper function:
+# TODO: delete or update cal_undistort()
 def cal_undistort(img, objpoints, imgpoints): # Use cv2.calibrateCamera() and cv2.undistort()
+    '''
+    # TODO : document cal_undistort()
+    :param img:
+    :param objpoints:
+    :param imgpoints:
+    :return:
+    '''
     nx, ny = 5, 5
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -67,52 +111,59 @@ def cal_undistort(img, objpoints, imgpoints): # Use cv2.calibrateCamera() and cv
     return undist
 
 # Helper function: undistort image
-def image_undistort(args, image, objpoints, imgpoints):
-    # Undistort using mtx and dist
-    undist = cv2.undistort(img, mtx, dist, None, mtx)
-    # 2) Convert to grayscale
-    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
-    # 3) Find the chessboard corners
+def image_undistort(args, image,  camera_matrix, coef_distorsion):
+    '''
+    # TODO : document image_undistort
+    #parameters
+    . args:
+    . image: array, (x,y,3)
+        - image in BRG format, i.e. read the file using cv2.imread()
+    . camera_matrix:
+    . coef_distorsion:
+
+    #return:
+    .
+    '''
+    # prepare object points
+    nx, ny = args.incorner #number of inside corners in x, in y
+
+    # undistort using camera_matrix and coef_distorsion
+    undist = cv2.undistort(image, camera_matrix, coef_distorsion, None, camera_matrix)
+    # Convert to grayscale
+    gray = cv2.cvtColor(undist, cv2.COLOR_RGB2GRAY)
+    # Find the chessboard corners
     ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-    # 4) If corners found:
+    # If corners found:
     if ret == True:
-        # a) draw corners
-        cv2.drawChessboardCorners(undist, (nx, ny), corners, ret)
-        # b) define 4 source points src = np.float32([[,],[,],[,],[,]])
+        # define 4 source points src = np.float32([[,],[,],[,],[,]])
         src = np.float32([ corners[0],corners[nx-1],corners[-1],corners[nx*(ny-1)] ])   # corners[-nx]]) # corners[nx*(ny-1)] ])
-                 #Note: you could pick any four of the detected corners
-                 # as long as those four corners define a rectangle
-                 #One especially smart way to do this would be to use four well-chosen
-                 # corners that were automatically detected during the undistortion steps
-                 #We recommend using the automatic detection of corners in your code
-        # c) define 4 destination points dst = np.float32([[,],[,],[,],[,]])
-        img_size = (gray.shape[1], gray.shape[0])
-        dist = np.float32([ [100, 100], [img_size[0]-100, 100], [img_size[0]-100, img_size[1]-100], [100, img_size[1]-100] ])
-        # d) use cv2.getPerspectiveTransform() to get M, the transform matrix
-        M = cv2.getPerspectiveTransform(src, dist)
-        # e) use cv2.warpPerspective() to warp your image to a top-down view
-        warped = cv2.warpPerspective(undist, M, img_size, flags=cv2.INTER_LINEAR)
-    #delete the next two lines
-    #M = None
-    #warped = np.copy(img)
+        # define 4 destination points dst = np.float32([[,],[,],[,],[,]])
+        image_size = (gray.shape[1], gray.shape[0])
+        coef_distorsion = np.float32([ [100, 100], [image_size[0]-100, 100], [image_size[0]-100, image_size[1]-100], [100, image_size[1]-100] ])
+        # use cv2.getPerspectiveTransform() to get M, the transform matrix
+        M = cv2.getPerspectiveTransform(src, coef_distorsion)
+        # use cv2.warpPerspective() to warp your image to a top-down view
+        warped = cv2.warpPerspective(undist, M, image_size, flags=cv2.INTER_LINEAR)
+
+
+    # Return the resulting image and matrix
     return warped, M
-
-
 
 def main():
     # parameters and placeholders
     args = PARSE_ARGS()
-    objpoints, imgpoints = camera_calibrate(args)
 
-    # return the camera matrix, distortion coefficients, rotation and translation vectors
-    corners_found, camera_matrix, coef_distorsion, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    # - corners_found : boolean, true if chesscorners have been found
-    # - camera_matrix : camera metrics to transform 3D objects to 2D images
-    # - coef_distorsion: distorsion coefficient
-    # - rvecs, tvecs: location of the camera in the world (r: rotation, t: translation)
+    # compute the camera matrix, distortion coefficients
+    camera_matrix, coef_distorsion = camera_calibrate(args)
 
+    # undistorted image
+    image = mpimg.imread(args.cali+'calibration1.jpg')
+    image_rectified, matrix_transform = image_undistort(args, image,  camera_matrix, coef_distorsion)
 
-    # image, mtx, dist -> image_undistort
+    # show undist image
+    plt.imshow(image_rectified)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
